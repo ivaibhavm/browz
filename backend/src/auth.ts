c736811f41c08f52f08c27a1fbd63c5a2d42b032
Vaibhav <vaibhavmishra020105@gmail.com>
2026-03-02 16:12:28 +0530

    Imporve auth and add Name column for signup

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -7,9 +7,9 @@ const router = express.Router()
 const prisma = new PrismaClient()
 
 router.post("/signup", (async (req, res) => {
-  const { email, password } = req.body
-  if (!email || !password)
-    return res.status(400).json({ error: "Email and password required" })
+  const { email, password, name } = req.body
+  if (!email || !password || !name)
+    return res.status(400).json({ error: "Please provide all the required fields" })
 
   try {
     const existingUser = await prisma.user.findUnique({ where: { email } })
@@ -18,7 +18,7 @@ router.post("/signup", (async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10)
     const user = await prisma.user.create({
-      data: { email, passwordHash: hashedPassword },
+      data: { email, passwordHash: hashedPassword, name },
     })
 
     const token = jwt.sign(
@@ -34,57 +34,57 @@ router.post("/signup", (async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    res.status(200).json({ message: "User created successfully"})
+    res.status(200).json({ message: "User created successfully" })
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: "Server error" })
   }
-})as RequestHandler)
+}) as RequestHandler)
 
 
 router.post("/login", (async (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password)
-      return res.status(400).json({ error: "Email and password required" })
-  
-    try {
-      const user = await prisma.user.findUnique({ where: { email } })
-      if (!user) return res.status(401).json({ error: "Invalid credentials" })
-  
-      const ok = await bcrypt.compare(password, user.passwordHash)
-      if (!ok) return res.status(401).json({ error: "Invalid credentials" })
-  
-      const token = jwt.sign(
-        { id: user.id, email: user.email },
-        process.env.JWT_SECRET!,
-      );
+  const { email, password } = req.body
+  if (!email || !password)
+    return res.status(400).json({ error: "Email and password required" })
 
-      console.log("token: " + token)
+  try {
+    const user = await prisma.user.findUnique({ where: { email } })
+    if (!user) return res.status(401).json({ error: "Invalid credentials" })
 
-      res.cookie("token", token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "lax",
-        path: "/",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      });
-  
-      res.status(200).json({ message: "Login successful", token})
-    } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: "Server error", err })
-    }
-})as RequestHandler)
+    const ok = await bcrypt.compare(password, user.passwordHash)
+    if (!ok) return res.status(401).json({ error: "Invalid credentials" })
 
-  
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET!,
+    );
+
+    console.log("token: " + token)
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ message: "Login successful", token })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Server error", err })
+  }
+}) as RequestHandler)
+
+
 router.get("/me", (async (req, res) => {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ user: "there is no token present" });
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ user: "there is no token present" });
 
-    const secret = process.env.JWT_SECRET as string | undefined
-    if (!secret) return res.status(500).json({ user: null, error: "Missing JWT secret" })
-    const decoded = jwt.verify(token, secret) as { id: string }
-    res.json({ user: decoded });
-})as RequestHandler)
+  const secret = process.env.JWT_SECRET as string | undefined
+  if (!secret) return res.status(500).json({ user: null, error: "Missing JWT secret" })
+  const decoded = jwt.verify(token, secret) as { id: string }
+  res.json({ user: decoded });
+}) as RequestHandler)
 
 export default router
