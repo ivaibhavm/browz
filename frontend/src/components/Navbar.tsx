31372cf8e64476f981411e5301014e2543485deb
Vaibhav <vaibhavmishra020105@gmail.com>
2026-03-06 16:38:02 +0530

    minor changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -42,7 +42,7 @@ export function Navbar() {
   const handleCloseAuthForm = () => {
     setShowAuthForm(false);
   };
-  
+
   useEffect(() => {
     if (shapeTimeoutRef.current) {
       clearTimeout(shapeTimeoutRef.current);
@@ -65,73 +65,77 @@ export function Navbar() {
 
   const logo = (
     <div className='text-white'>
-        <Link to="/">
-            <img src='logo.png' height={40} width={40}></img>
-        </Link>
+      <Link to="/">
+        <img src='logo.png' height={40} width={40}></img>
+      </Link>
     </div>
   );
 
   const navLinksData = [
-    { label: 'Contact', href: 'https://x.com/mvahbiav'},
+    { label: 'Contact', href: 'https://x.com/mvahbiav' },
     { label: 'Github', href: 'https://github.com/ivaibhavm/browz' },
   ];
 
   return (
     <>
-    <header className={`fixed top-6 left-1/2 transform -translate-x-1/2 z-20 flex flex-col items-center pl-6 pr-6 py-3 backdrop-blur-sm ${headerShapeClass} border border-[#333] bg-indigo-500/6 w-[calc(100%-2rem)] md:w-[800px] transition-[border-radius] duration-0 ease-in-out`}>
+      <header className={`fixed top-6 left-1/2 transform -translate-x-1/2 z-20 flex flex-col items-center pl-6 pr-6 py-3 backdrop-blur-sm ${headerShapeClass} border border-[#333] bg-indigo-500/6 w-[calc(100%-2rem)] md:w-[800px] transition-[border-radius] duration-0 ease-in-out`}>
 
-      <div className="flex items-center justify-between w-full gap-x-6 sm:gap-x-8">
-        <div className="flex items-center">
-           {logo}
+        <div className="grid grid-cols-3 items-center w-full gap-x-6 sm:gap-x-8">
+          <div className="flex items-center">
+            {logo}
+          </div>
+
+          <nav className={`hidden sm:flex items-center space-x-4 sm:space-x-6 text-sm ${user ? 'mx-auto' : ''}`}>
+            {navLinksData.map((link) => (
+              <AnimatedNavLink key={link.href} href={link.href}>
+                {link.label}
+              </AnimatedNavLink>
+            ))}
+          </nav>
+
+          <div className='justify-self-end'>
+            {user ?
+              <div>
+                <User className='text-gray-300' />
+              </div>
+              :
+              <div className="hidden sm:flex justify-end items-center gap-2 sm:gap-3 max-w-48 w-full">
+                <Button className='bg-white text-black hover:bg-slate-100 w-20' onClick={handleLogin}> Login </Button>
+                <Button className='bg-white text-black hover:bg-slate-100 w-20' onClick={handleSignup}> Signup </Button>
+              </div>
+            }
+          </div>
+
+          <button className="sm:hidden flex items-center justify-center w-8 h-8 text-gray-300 focus:outline-none" onClick={toggleMenu} aria-label={isOpen ? 'Close Menu' : 'Open Menu'}>
+            {isOpen ? (
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+            ) : (
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            )}
+          </button>
         </div>
 
-        <nav className="hidden sm:flex items-center space-x-4 sm:space-x-6 text-sm">
-          {navLinksData.map((link) => (
-            <AnimatedNavLink key={link.href} href={link.href}>
-              {link.label}
-            </AnimatedNavLink>
-          ))}
-        </nav>
-
-        {user ? 
-            <div>
-                <User className='text-gray-300' />
-            </div>
-          :
-            <div className="hidden sm:flex items-center gap-2 sm:gap-3">
-                <Button className='bg-white text-black hover:bg-slate-100' onClick={handleLogin}> Login </Button>
-                <Button className='bg-white text-black hover:bg-slate-100' onClick={handleSignup}> Signup </Button>
-            </div>
-        }
-
-        <button className="sm:hidden flex items-center justify-center w-8 h-8 text-gray-300 focus:outline-none" onClick={toggleMenu} aria-label={isOpen ? 'Close Menu' : 'Open Menu'}>
-          {isOpen ? (
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-          ) : (
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          )}
-        </button>
-      </div>
-
-      <div className={`sm:hidden flex flex-col items-center w-full transition-all ease-in-out duration-300 overflow-hidden
+        <div className={`sm:hidden flex flex-col items-center w-full transition-all ease-in-out duration-300 overflow-hidden
                        ${isOpen ? 'max-h-[1000px] opacity-100 pt-4' : 'max-h-0 opacity-0 pt-0 pointer-events-none'}`}>
-        <nav className="flex flex-col items-center space-y-4 text-base w-full">
-          {navLinksData.map((link) => (
-            <a key={link.href} href={link.href} className="text-gray-300 hover:text-white transition-colors w-full text-center">
-              {link.label}
-            </a>
-          ))}
-        </nav>
-      </div>
-    </header>
-    {/* Auth Form */}
-    {showAuthForm && (
-      <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/40 backdrop-blur-md" onClick={handleCloseAuthForm}>
-        <Card className="max-w-md w-full mx-4 relative bg-[#191919] border border-[#333] shadow-lg backdrop-blur-none z-[101]" onClick={e => e.stopPropagation()}>
-          <AuthForm type={authType} onToggleType={() => setAuthType(prev => prev === 'login' ? 'signup' : 'login')} onSuccess={() => {setShowAuthForm(false); refreshUser(); }}/>
-        </Card>
-      </div>
-    )}
+          <nav className="flex flex-col items-center space-y-4 text-base w-full">
+            {navLinksData.map((link) => (
+              <a key={link.href} href={link.href} className="text-gray-300 hover:text-white transition-colors w-full text-center">
+                {link.label}
+              </a>
+            ))}
+            <Button className='bg-white text-black hover:bg-slate-100 max-w-20 w-full' onClick={handleLogin}> Login </Button>
+            <Button className='bg-white text-black hover:bg-slate-100 max-w-20 w-full' onClick={handleSignup}> Signup </Button>
+          </nav>
+        </div>
+      </header>
+      {/* Auth Form */}
+      {showAuthForm && (
+        <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/40 backdrop-blur-md" onClick={handleCloseAuthForm}>
+          <Card className="max-w-md w-full mx-4 relative bg-[#191919] border border-[#333] shadow-lg backdrop-blur-none z-[101]" onClick={e => e.stopPropagation()}>
+            <AuthForm type={authType} onToggleType={() => setAuthType(prev => prev === 'login' ? 'signup' : 'login')} onSuccess={() => { setShowAuthForm(false); refreshUser(); }} />
+          </Card>
+        </div>
+      )}
     </>
   );
 }
